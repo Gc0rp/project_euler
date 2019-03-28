6f4a740dc1db52fc96369376bd5b2a435cf778c8
OJAS GUPTA <102542177@student.swin.edu.au>
2019-03-28 15:37:36 +1100

    Added a new problem

---
+++ problem1/problem1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 102542177
+ * Date: 28/03/2019
+ * Time: 3:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace problem1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int number = 0, total = 0;
+			
+			while(number < 1000){
+				if ( (number % 3 == 0) || number % 5 == 0){
+					total += number;
+				}	
+				
+				number++;
+			}
+			
+			Console.WriteLine(total);
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
